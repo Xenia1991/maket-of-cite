f425b2fefe18a1b42fe25dd87939964678c668ab
Xenia <andreeva19-91@mail.ru>
2024-01-30 00:32:55 +0700

    добавлен пятый блок

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-// import '../scss/style.scss';
-// import './block-2';
-// import './block-3';
-//import './block-4';
+import '../scss/style.scss';
+import './block-2';
+import './block-3';
+import './block-4';
