7900ba64a807a421083fe7d2e44086f510690a39
Xenia <andreeva19-91@mail.ru>
2024-01-31 17:57:39 +0700

    небольшие поправки по стилю

--- src/js/block-3.js
+++ src/js/block-3.js
@@ -1,64 +1,64 @@
-// let repairSwiper;
-// let repairContainerElement = document.querySelector('.repair-list');
-// let buttonElement = document.querySelector('.button');
-// let pageContainerElement = document.querySelector('.third-section-container');
+let repairSwiper;
+let repairContainerElement = document.querySelector('.repair-list');
+let buttonElement2 = document.querySelector('.button-second');
+let pageContainerElement = document.querySelector('.third-section-container');
 
-// if (pageContainerElement.clientWidth < 768) {
-//     repairSwiper = new Swiper('.swiper', {
+if (pageContainerElement.clientWidth < 768) {
+    repairSwiper = new Swiper('.swiper', {
 
-//        spacebetween: '16',
+       spacebetween: '16',
 
-//        slidesPerView: '1.13',
+       slidesPerView: '1.13',
 
-//        pagination: {
-//           el: '.swiper-pagination',
-//           clickable: true,
-//        }
-//     });
-//  };
+       pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+       }
+    });
+ };
 
-//  let resizeWindow = function () {
-//     if (pageContainerElement.clientWidth < 768 && !repairSwiper) {
-//        repairSwiper = new Swiper('.swiper', {
-//           slidesPerView: '1.2',
+ let windowResizing = function () {
+    if (pageContainerElement.clientWidth < 768 && !repairSwiper) {
+       repairSwiper = new Swiper('.swiper', {
+          slidesPerView: '1.2',
 
-//           pagination: {
-//              el: '.swiper-pagination',
-//              clickable: true,
-//           }
-//        });
-//     } else if (pageContainerElement.clientWidth>=768 && repairSwiper) {
-//        repairSwiper.disable();
-//        repairSwiper.destroy(false, true);
-//        repairSwiper = null;
-//     }
-//  }
+          pagination: {
+             el: '.swiper-pagination',
+             clickable: true,
+          }
+       });
+    } else if (pageContainerElement.clientWidth>=768 && repairSwiper) {
+       repairSwiper.disable();
+       repairSwiper.destroy(false, true);
+       repairSwiper = null;
+    }
+ }
 
-// let openRepairMenu = function () {
-//     if (repairContainerElement.classList.contains('repair-list--hidden')) {
+let openRepairMenu = function () {
+    if (repairContainerElement.classList.contains('repair-list--hidden')) {
 
-//         repairContainerElement.classList.remove('repair-list--hidden');
-//         repairContainerElement.classList.add('repair-list--unhidden');
+        repairContainerElement.classList.remove('repair-list--hidden');
+        repairContainerElement.classList.add('repair-list--unhidden');
 
-//         let buttonText = buttonElement.querySelector('.button__text');
-//         buttonText.textContent = 'Скрыть';
+        let buttonText = buttonElement2.querySelector('.button-second__text');
+        buttonText.textContent = 'Скрыть';
 
-//         let buttonImg = buttonElement.querySelector('.button__img');
-//         buttonImg.src = '/images/up.png';
+        let buttonImg = buttonElement2.querySelector('.button-second__img');
+        buttonImg.src = './img/up.png';
 
-//     } else if (repairContainerElement.classList.contains('repair-list--unhidden')) {
+    } else if (repairContainerElement.classList.contains('repair-list--unhidden')) {
 
-//         repairContainerElement.classList.remove('repair-list--unhidden');
-//         repairContainerElement.classList.add('repair-list--hidden');
+        repairContainerElement.classList.remove('repair-list--unhidden');
+        repairContainerElement.classList.add('repair-list--hidden');
 
-//         let buttonText = buttonElement.querySelector('.button__text');
-//         buttonText.textContent = 'Показать все';
+        let buttonText = buttonElement2.querySelector('.button-second__text');
+        buttonText.textContent = 'Показать все';
 
-//         let buttonImg = buttonElement.querySelector('.button__img');
-//         buttonImg.src = '/images/expand.png';
+        let buttonImg = buttonElement2.querySelector('.button-second__img');
+        buttonImg.src = './img/expand.png';
 
-//     }
-// }
+    }
+}
 
-//  window.addEventListener ('resize', resizeWindow);
-//  buttonElement.addEventListener('click', openRepairMenu);
+ window.addEventListener ('resize', windowResizing);
+ buttonElement2.addEventListener('click', openRepairMenu);
--- src/js/block-4.js
+++ src/js/block-4.js
@@ -1,13 +1,13 @@
-// let priceSwiper;
-// let pageContainerElement = document.querySelector('.forth-section-container');
+let priceSwiper;
+let pageContainerElement1 = document.querySelector('.forth-section-container');
 
-// priceSwiper = new Swiper('.swiper', {
-//     slidesPerView: '1.13',
+priceSwiper = new Swiper('.swiper', {
+    slidesPerView: '1.13',
 
-//     spaceBetween: '10',
+    spaceBetween: '10',
 
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//     },
-// })
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+})
